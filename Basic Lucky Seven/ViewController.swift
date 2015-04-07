713063939a3ffbce626e7b41b2ac3a4469dc03d1
Henry Pham <Henry@Vivians-MacBook-Air.local>
2015-04-06 18:58:45 -0700

    made fully functional

--- Basic Lucky Seven/ViewController.swift
+++ Basic Lucky Seven/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumberLabel: UILabel!
+    @IBOutlet weak var secondNumberLabel: UILabel!
+    @IBOutlet weak var thirdNumberLabel: UILabel!
+    
+    @IBOutlet weak var spinButton: UIButton!
+    
+    @IBOutlet weak var winsLabel: UILabel!
+    @IBOutlet weak var lossesLabel: UILabel!
+    @IBOutlet weak var totalGamesLabel: UILabel!
+
+    var wins:Int = 0
+    var losses:Int = 0
+    var total:Int = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +35,52 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func spinButton(sender: UIButton) {
+        
+        var sevenCount = 0
+        firstNumberLabel.hidden = false
+        secondNumberLabel.hidden = false
+        thirdNumberLabel.hidden = false
+        
+        let firstNumber = Int(arc4random_uniform(UInt32(8)))
+        let secondNumber = Int(arc4random_uniform(UInt32(8)))
+        let thirdNumber = Int(arc4random_uniform(UInt32(8)))
+        
+        if firstNumber == 7{
+        sevenCount += 1
+        }
+        else {
+        }
+        
+        if secondNumber == 7{
+        sevenCount += 1
+        }
+        else{
+        }
+        
+        if thirdNumber == 7{
+        sevenCount += 1
+        }
+        else{
+        }
+        
+        if sevenCount == 3 {
+        wins += 1
+        }
+        else{
+        losses += 1
+        }
+        
+        total += 1
+        
+        firstNumberLabel.text = "\(firstNumber)"
+        secondNumberLabel.text = "\(secondNumber)"
+        thirdNumberLabel.text = "\(thirdNumber)"
+        winsLabel.text = "Wins: \(wins)"
+        lossesLabel.text = "Losses: \(losses)"
+        totalGamesLabel.text = "Total Games: \(total)"
+    
+    }
 
 }
 
